d9fc50e99b4272c970866c47e191cecbc9d111e5
hdinhof1 <hdinhof1@binghamton.edu>
2016-07-06 10:20:08 -0400

    Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,9 +14,10 @@
  ### 1. Create a function that will take in a continent and the number of countries in that continent. It should print the following sentence "____ is a continent which contains ___ countries".
  */
 // write your code here
-
-
-
+func displayContinentWithCountries (continent: String, countries: Int) {
+    print("\(continent) is a continent which contains \(countries) countries")
+}
+displayContinentWithCountries("Asia", countries: 48) //+ 6 territories, so fun fact I just learned, Asia has the same number of countries as Africa
 
 /*: question2
  ### 2. Can you see why the code below doesn't work? Fix the function call to get rid of the error.
@@ -25,15 +26,12 @@ func greeting(name: String, greeting: String) {
     print("\(greeting), \(name)!")
 }
 
-greeting("Danny", "Hello")
-
-
-
+greeting("Danny", greeting:"Hello")
 
 /*: question3
  ### 3. This function doesn't work, either. Can you fix the function (_not_ the call) so that it works?
  */
-func daysInMonth(month: String, days: String) {
+func daysInMonth(month: String, days: Int) {
     print("There are \(days) in \(month)")
 }
 
@@ -46,8 +44,10 @@ daysInMonth("November", days: 30)
  ### 4. So far, you have created functions that take two arguments. Can you create (and call) one that takes three? Try to create a function that three parameters: a beverage, the number of bottles of that beverage, and a place you can keep those bottles. Print the sentence "____ bottles of ____ on the ____ wall."
  */
 // write your code here
-
-
+func NinetyNineBottlesOfBurr(a beverage: String, numberOfBottles: Int, placeToKeepBottles: String) {
+    print ("\(numberOfBottles) bottles of \(beverage) on the \(placeToKeepBottles) wall.")
+}
+NinetyNineBottlesOfBurr(a: "Beer", numberOfBottles: 99, placeToKeepBottles: "back")
 
 
 /*:
